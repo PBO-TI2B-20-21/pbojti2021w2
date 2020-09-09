88b613b5738e28d37c8436f7ae12f996f75dd053
gustijuno <gustijuno@gmail.com>
2020-09-09 16:52:02 +0700

    Tugas

---
+++ Tugas/Game/Game/src/PinjamMain.java
@@ -0,0 +1,13 @@
+public class PinjamMain {
+    public static void main(String[] args) {
+        Pinjam pjm=new Pinjam();
+        pjm.idMbr=1124542;
+        pjm.namaMbr="Gusti Herjuno";
+        pjm.namaGame="The Witcher 3";
+        pjm.harga=50000;
+        pjm.lamaSewa=3;
+        pjm.tampilData();
+
+
+    }
+}
