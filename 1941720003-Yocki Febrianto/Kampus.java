9e23493d367a116aeb0c126921bcb95130f3fa79
yocki24 <44716112+yocki24@users.noreply.github.com>
2020-09-09 16:48:17 +0700

    Add files via upload

---
+++ 1941720003-Yocki Febrianto/Kampus.java
@@ -0,0 +1,9 @@
+public class Kampus {
+    public String namaKampus, jurusan, prodi;
+
+    public void tampil(){
+        System.out.println("Nama Kampus\t: " + namaKampus);
+        System.out.println("Jurusan\t\t: " + jurusan);
+        System.out.println("Prodi\t\t: " + prodi);
+    }
+}
---
+++ 1941720003-Yocki Febrianto/Sekolah.java
@@ -0,0 +1,7 @@
+public class Sekolah {
+    public String namaSekolah, alamatSekolah;
+    public void tampilBiodata(){
+        System.out.println("Nama Sekolah \t: " + namaSekolah);
+        System.out.println("Alamat Sekolah\t: " + alamatSekolah);
+    }
+}
